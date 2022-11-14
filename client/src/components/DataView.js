5294845da74f5c093f4ed183893fc6569b694746
Kelly Fang <kellyfangg@gmail.com>
2022-11-14 01:49:59 -0500

    style individual data pop up

--- client/src/components/DataView.js
+++ client/src/components/DataView.js
@@ -1,8 +1,29 @@
 import React, { useEffect, useState, useCallback} from 'react';
 
+const CATEGORY_OPTIONS = [
+    "Illegal Dumping",
+    "Rubbish and Recycling",
+    "Abandoned Vehicle",
+    "Pothole Repair",
+    "Graffiti Removal",
+    "Vacant Lots",
+    "Street Light Outage",
+    "Property Maintenance",
+    "Street Trees",
+    "Other",
+  ];
+
 export default function DataView ({}) {
     return (
         <div className="card card-style">
+            <div className="data-container"> 
+                <div className="data-section"> 
+                    <p className="data-title">Number of Requests</p>
+                </div>
+                <div className="data-section"> 
+                    <p className="data-title">Requests Completed</p>
+                </div>
+            </div>
 
         </div>
     );
